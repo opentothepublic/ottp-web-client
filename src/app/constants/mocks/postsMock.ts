c19bcefade84a00135315c2e31aa8e31bc3b0c6f
mattweinstein <matt.weinstein@childrens.harvard.edu>
2024-08-05 23:01:07 -0600

    updates mock

--- src/app/constants/mocks/postsMock.ts
+++ src/app/constants/mocks/postsMock.ts
@@ -2,10 +2,10 @@ import { Post } from "@/app/components/Post";
 import { ProjectProposal } from "@/components/index";
 
 const post1: Post = {
-  name: "Alice",
-  collaborators: ["@alice", "@bob"],
+  name: "undefined",
+  collaborators: ["@undefined", "@animated", "@statuette"],
   description:
-    "This is an example desciription. it might be long and go on two lines. This is an example desciription. it might be long and go on two lines",
+    "This week I also built a website for basecamp lawn games - dunkathon. participants could play to earn onchain points for a chance to dunk @jessepollak. learned an important lesson to make end user experience easier and smoother. kudos to @animated...",
   timeElapsed: "3m",
   projectsAndProposals: [
     { name: "Project 1", type: ProjectProposal.PROJECT },
@@ -14,19 +14,19 @@ const post1: Post = {
 };
 
 const post2: Post = {
-  name: "Charlie",
-  collaborators: ["@alice", "@bob"],
+  name: "Toady Hawk",
+  collaborators: ["@toadyhawk.eth", "@robin"],
   description:
-    "This is an example desciription. it might be long and go on two lines. This is an example desciription. it might be long and go on two lines",
+    "This week I built and launched a new Yellow Collective creator contest, in collaboration with our friends over at @moshicam, where we are offering a prize pool of 1 ETH to be distributed via @rounds to any shutterbugs who snap an Onchain Summer...",
   timeElapsed: "8m",
   projectsAndProposals: [{ name: "Proposal", type: ProjectProposal.PROPOSAL }],
 };
 
 const post3: Post = {
-  name: "Bob",
-  collaborators: ["@alice", "@charlie"],
+  name: "robin",
+  collaborators: ["@robin", "@kaito"],
   description:
-    "This is an example desciription. it might be long and go on two lines. This is an example desciription. it might be long and go on two lines",
+    "sign up for an app, mint things onchain, earn ETH, and withdraw to your personal wallet to use in the broader crypto economy – without ever depositing crypto or knowing what a seed phrase is. this is possible with our latest /moshicam update!",
   timeElapsed: "11m",
   projectsAndProposals: [
     { name: "Project", type: ProjectProposal.PROJECT },
