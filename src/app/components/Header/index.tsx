b363d6ca6ab3a3f94de1a3c86d4be055535fc180
mattweinstein <matt.weinstein@childrens.harvard.edu>
2024-11-04 05:40:59 -0700

    adds color scheme from figma

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 export const Header: React.FC<PropsWithChildren<{}>> = ({ children }) => {
   return (
     <div className="flex justify-between">
-      <div className="text-2xl text-blue-800">ottp://</div>
+      <div className="text-2xl text-blue">ottp://</div>
       <button className="p-2 bg-black text-white">SIGN IN</button>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,31 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      black: "#000000",
+      blue: "#0000FF",
+      dodgerBlue: "1E90FF",
+      green: "#008000",
+      forestGreen: "#228B22",
+      orange: "#FFA500",
+      purple: "#800080",
+      mediumOrchid: "#BA55D3",
+      red: "#FF0000",
+      white: "#FFFFFF",
+      gray: {
+        950: "#0D0D0D",
+        900: "#1A1A1A",
+        800: "#333333",
+        700: "#4D4D4D",
+        600: "#666666",
+        500: "#808080",
+        400: "#999999",
+        300: "#B3B3B3",
+        200: "#CCCCCC",
+        100: "#E6E6E6",
+        50: "#F2F2F2",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
