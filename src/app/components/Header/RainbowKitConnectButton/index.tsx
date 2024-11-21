03de72c87ef0811d2ea63ce1c49ad6ca3ddca1a9
mattweinstein <matt.weinstein@childrens.harvard.edu>
2024-11-20 21:28:08 -0500

    implements rainbowkit

---
+++ src/app/components/Header/RainbowKitConnectButton/index.tsx
@@ -0,0 +1,8 @@
+import { PropsWithChildren } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export const RainbowKitConnectButton: React.FC<
+  PropsWithChildren<{}>
+> = ({}) => {
+  return <ConnectButton />;
+};
