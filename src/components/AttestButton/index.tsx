2f9ba81ebeaed763dd372c30b62b19568f345218
mattweinstein <matt.weinstein@childrens.harvard.edu>
2024-12-17 15:00:04 -0700

    attestation works

--- src/components/AttestButton/index.tsx
+++ src/components/AttestButton/index.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
 import { Button } from "../common/Button";
-import { connectToEAS } from "@/utils/blockchain/connectToEAS";
+import { attestOnChain } from "@/utils/blockchain/connectToEAS";
 
 export const AttestButton: React.FC = () => {
-  return <Button onClick={connectToEAS}></Button>;
+  return <Button onClick={attestOnChain}></Button>;
 };
--- src/utils/blockchain/connectToEAS.ts
+++ src/utils/blockchain/connectToEAS.ts
@@ -7,25 +7,20 @@ import {
 } from "@ethereum-attestation-service/eas-sdk";
 import { ethers, toBigInt } from "ethers";
 
-const setupSigner = async () => {
+const EASBaseSepoliaContractAddress =
+  "0x4200000000000000000000000000000000000021";
+
+export const initializeEAS = async () => {
   if (window.ethereum) {
     try {
-      await window.ethereum.request({ method: "eth_requestAccounts" });
+      const eas = new EAS(EASBaseSepoliaContractAddress);
 
-      // Create an ethers provider using the browser's Ethereum provider (MetaMask)
-      const provider = await new ethers.BrowserProvider(window.ethereum);
+      const provider = new ethers.BrowserProvider(window.ethereum);
 
-      // Get the signer (user's wallet)
-      const signer = await provider.getSigner();
-
-      // Get the address of the signer (user's wallet address)
-      const signerAddress = await signer.getAddress();
-      console.log("Signer address:", signerAddress);
-      return signer;
+      eas.connect(provider);
     } catch (e) {
       console.error("Error connecting to wallet:", e);
     }
-    // Request account access (only needed once in the browser)
   } else {
     console.error(
       "No ethereum provider found. Please install one (Metamask,Phantom,etc.) "
@@ -33,65 +28,47 @@ const setupSigner = async () => {
   }
 };
 
-export const connectToEAS = async () => {
+export const attestOnChain = async () => {
   try {
-    const connectedSigner = await setupSigner();
-    if (!connectedSigner) {
-      throw new Error("no address found");
+    if (window.ethereum) {
+      const eas = new EAS(EASBaseSepoliaContractAddress);
+
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+
+      const signer = await provider.getSigner();
+
+      eas.connect(signer);
+
+      const schemaEncoder = new SchemaEncoder(
+        "uint256 fid, uint256 action, string data, uint8 label, bytes tags"
+      );
+      const encodedData = schemaEncoder.encodeData([
+        { name: "fid", value: toBigInt(2), type: "uint256" },
+        { name: "action", value: 1, type: "uint256" },
+        { name: "data", value: "Testdata", type: "string" },
+        { name: "label", value: 2, type: "uint8" },
+        { name: "tags", value: "0x3039", type: "bytes" },
+      ]);
+
+      const schemaUID =
+        "0x9e17d50ab0011c5816db3d3eb79866f1cf66e2933e6ce76199679225ab6cd811";
+
+      const transaction = await eas.attest({
+        schema: schemaUID,
+        data: {
+          recipient: "0x0000000000000000000000000000000000000000",
+          expirationTime: NO_EXPIRATION,
+          revocable: true,
+          data: encodedData,
+        },
+      });
+
+      const newAttestationUID = await transaction.wait();
+
+      console.log("New attestation UID:", newAttestationUID);
     }
-
-    // Sepolia - https://docs.attest.org/docs/quick--start/contracts
-    const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e";
-
-    // Initialize the sdk with the address of the EAS Schema contract address
-    const eas = new EAS(EASContractAddress);
-
-    // Gets a default provider (in production use something else like infura/alchemy)
-
-    // Connects an ethers style provider/signingProvider to perform read/write functions.
-    // MUST be a signer to do write operations!
-    eas.connect(connectedSigner);
-
-    // Initialize SchemaEncoder with the schema string
-    const schemaEncoder = new SchemaEncoder(
-      "uint256 fid, uint256 action, string data, uint8 label, bytes tags"
-    );
-    const encodedData = schemaEncoder.encodeData([
-      { name: "fid", value: 2, type: "uint256" },
-      { name: "action", value: 2, type: "uint256" },
-      { name: "data", value: "Test data", type: "string" },
-      { name: "label", value: 2, type: "uint8" },
-      { name: "tags", value: "0x3039", type: "bytes" },
-    ]);
-
-    const schemaUID =
-      "0x9e17d50ab0011c5816db3d3eb79866f1cf66e2933e6ce76199679225ab6cd811";
-
-    const phantomTestWallet = "0x65e0b133e2e1a28b2ac5130e8e2588d209c084cf";
-
-    const transaction = await eas.attest({
-      schema: schemaUID,
-      data: {
-        recipient: phantomTestWallet,
-        expirationTime: NO_EXPIRATION,
-        revocable: true, // Be aware that if your schema is not revocable, this MUST be false
-        data: encodedData,
-        // value: toBigInt(10000),
-      },
-    });
-
-    const newAttestationUID = await transaction.wait();
-
-    // console.log("New attestation UID:", newAttestationUID);
-
-    console.log("Transaction receipt:", transaction);
   } catch (e) {
-    if (e instanceof Error) {
-      console.error("Error message:", e.message);
-      console.error("Stack trace:", e.stack);
-      console.error("Error name:", e.name);
-    } else {
-      console.error("Unexpected error:", e);
-    }
+    console.error(e);
   }
 };
