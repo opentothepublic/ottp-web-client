e08df276947216740a3d8d38e457346297f188e7
mattweinstein <matt.weinstein@childrens.harvard.edu>
2024-12-17 21:56:47 -0700

    implements RHF

--- src/components/AttestationDialog/index.tsx
+++ src/components/AttestationDialog/index.tsx
@@ -1,18 +1,30 @@
 import { Button, ButtonVariant } from "@/components/common/Button";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import { PropsWithChildren, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
 
 const formLabelClass = "title-small mb-6";
 const formParagraphClass = "my-2";
 const formSection = "my-6";
 
+type Inputs = {
+  collaborators: string;
+  contributon: string;
+  ethereumAddress: string;
+};
+
 export const AttestationDialog = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    trigger,
+  } = useForm<Inputs>();
   const [open, setOpen] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -22,25 +34,16 @@ export const AttestationDialog = () => {
     setOpen(false);
   };
 
+  const revalidateForm = async () => {
+    const isValid = await trigger();
+    setIsFormValid(isValid);
+  };
+
   return (
     <>
       <Button onClick={handleClickOpen}>Open form dialog</Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          component: "form",
-          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries((formData as any).entries());
-            const email = formJson.email;
-            console.log(email);
-            handleClose();
-          },
-        }}
-      >
-        <form className="body-medium p-12">
+      <Dialog open={open} onClose={handleClose}>
+        <form className="body-medium p-12" onSubmit={handleSubmit(onSubmit)}>
           <aside className="body-small">ADD CONTRIBUTION</aside>
           <div className={formSection}>
             <label className={formLabelClass}>Project</label>
@@ -53,8 +56,10 @@ export const AttestationDialog = () => {
               @username @username. Separate each with a space.
             </p>
             <input
+              {...register("collaborators", { required: true })}
               className="w-full pl-6 py-2 border"
               placeholder="@lowcodekrish @naaate @decipher @ting"
+              onBlur={revalidateForm} // Trigger validation for all fields on blur
             />
           </div>
           <div className={formSection}>
@@ -62,20 +67,30 @@ export const AttestationDialog = () => {
             <p className={formParagraphClass}>
               Described what you worked on together.
             </p>
-            <textarea className="w-full pl-6 py-2 border" placeholder="Built" />
+            <textarea
+              {...register("contributon", { required: true })}
+              className="w-full pl-6 py-2 border"
+              placeholder="Built"
+              onBlur={revalidateForm} // Trigger validation for all fields on blur
+            />
           </div>
           <div className={formSection}>
             <label className="title-small">Ethereum Address</label>
             <p className={formParagraphClass}>
               Select an Ethereum address to attest from
             </p>
-            <input className="w-full pl-6 py-2 border" placeholder="Built" />
+            <input
+              {...register("ethereumAddress", { required: true })}
+              className="w-full pl-6 py-2 border"
+              placeholder="Built"
+              onBlur={revalidateForm} // Trigger validation for all fields on blur
+            />
           </div>
           <p>By attesting you are confirming onchain.</p>
 
           <Button
             className="float-right"
-            variant={ButtonVariant.IDLE}
+            variant={isFormValid ? ButtonVariant.MAIN : ButtonVariant.IDLE}
             type="submit"
           >
             Add
